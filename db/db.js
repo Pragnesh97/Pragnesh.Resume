58bca208deffd1a79ab9e41efb7710a742c93884
Kaushik <kvs1101999@gmail.com>
2024-06-08 11:59:21 +0530

    init

---
+++ db/db.js
@@ -0,0 +1,229 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Pragnesh Sutariya. Currently, i’m a Software Engineer at SCIKEY, Surat. I'm specialized in building C# Softwares . I have completed my Diploma in Computer Application from Rabindranath Tagore University.",
+				"I love to Travel+Code+Music.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email at PragneshSutariya01@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Computer S/W Languages",
+			skillName: "C#, C++, C",
+			color: "1",
+			percentage: "90",
+		},
+		{
+			title: "Languages",
+			skillName: "Python",
+			color: "2",
+			percentage: "50",
+		},
+		{
+			title: "Mobile App Dev Languages",
+			skillName: "Flutter, Dart, Firebase",
+			color: "4",
+			percentage: "40",
+		},
+		{
+			title: "Database",
+			skillName: "Ms-SQL Server, MySql",
+			color: "3",
+			percentage: "85",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap",
+			color: "5",
+			percentage: "40",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub, SVN",
+			color: "6",
+			percentage: "70",
+		},
+		{
+			title: "Tools",
+			skillName: "Postman, XAMPP",
+			color: "2",
+			percentage: "75",
+		},
+		{
+			title: "Tools",
+			skillName: "Infragistics, Dev Express, SAP Crystal Reports, MS-Office",
+			color: "1",
+			percentage: "75",
+		}, 
+		{
+			title: "Editor",
+			skillName: "Visual Studio, Android Studio, VS Code",
+			color: "3",
+			percentage: "80",
+		},
+		{
+			title: "API",
+			skillName: "REST APIs, Documentation",
+			color: "4",
+			percentage: "40",
+		},
+	],
+	projects: {
+	
+		software: [
+			{
+				projectName: "Diamond Inventory System",
+				image: "images/PdiamonInvSys.png",
+				summary:
+					"It is a vast inventory and Diamond manufacturing process tracking system. It's a complete ERP package for Diamond manufacturing companies. DIS has been developed to incorporate all possibilities and permutations, combinations to address all needs of diamond manufacturing companies. DIS deals with all the requirement of Diamond Industries in managing very complex Diamond Manufacturing process.",
+				preview:
+					"",
+				techStack: ["c#", "SQL Server",".Net Framework","Infragistics","SAP Crystal Reports"],
+			},
+			{
+				projectName: "Textile Inventory System",
+				image: "images/pTexInvSys.jpg",
+				summary:
+					"Software to store and manage, calculate, and share textile information and generate bills. It gives prediction and full analysis of a data.",
+				preview: "",
+				techStack: ["c#", "SQL Server",".Net Framework","Infragistics","SAP Crystal Reports"],
+			},
+		],
+		
+		freelance: [
+			{
+				projectname: "snylloair.com",
+				image: "images/snylloair.png",
+				summary:
+					"developed a company website to showcase the purpose, services and products provided by the company to audience.",
+				preview: "https://www.snylloair.com/",
+				techstack: ["bootstrap", "javascript", "aws-s3"],
+			},
+			{
+				projectname: "delivery+",
+				image: "images/am-logo-.png",
+				summary:
+					"android application to display website in android devices.",
+				preview:
+					"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+				techstack: ["android", "java", "play store"],
+			},
+		],
+	},
+	experience: [
+		{
+			title: "Freelancer",
+			duration: "December 2021 - Present",
+			subtitle: "Flutter Developer",
+			details: [
+				"Worked as a Flutter developer to make client base Android App.",
+				"Developed a customer Mobile Application from scratch for Employee Task Management and build ETEX App, which is used in Textile business.",
+			],
+			tags: [
+				"Flutter",
+				"C#",
+				"Python",
+				"Multiple Programming languages",
+				
+			],
+			icon: "group",
+		},
+		{
+			title: "MR Diamond",
+			duration: "June 2023 - Present",
+			subtitle: "Software Engineer",
+			details: [
+				"Worked on Desktop Applications. APIs, SQL Server. The main projects are Diamond Inventory System, Textile Inventory System, Payroll. In which I have developed new, efficient, and well-tested code.",
+				"Crafted and Integrated new reports using SQL-Server. Coded software updates to add new features, close security holes, and resolve defects.",
+				"Co-ordinated closely with the product team, backend team, android team, and QA team to deliver the product builds before deadlines.",
+			],
+			tags: [
+				"C#",
+				"SQL-Server",
+				"Infragistics",
+				"Dev Express Control",
+				"SAP Crystal Reports",
+				"Github",
+			],
+			icon: "group",
+		},
+	],
+	education: [
+		{
+			title: "Diploma in Computer Application",
+			duration: "2017-2021",
+			subtitle: "Rabindranath Tagore University",
+			
+			tags: [
+				"Data Structures & Algorithms",
+				"Operating Systems",
+				"Database Management System"				
+			],
+			icon: "graduation-cap",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				// {
+				// 	text: "Stackoverflow",
+				// 	link: "https://stackoverflow.com/users/8461233/vinay-somawat",
+				// },
+				{
+					text: "GitHub",
+					link: "https://github.com/Pragnesh97",
+				},
+				// {
+				// 	text: "LeetCode",
+				// 	link: "https://leetcode.com/somawatvinay/",
+				// },
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+				
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.google.com/url?q=http://www.linkedin.com/in/pragnesh-sutariya-a835b317b&source=gmail&ust=1717868133051000&usg=AOvVaw0W3L5m1Ydosfqb5q508_oh",
+				},
+				// {
+					// text: "Instagram",
+					// link: "https://www.instagram.com/Pragnesh_sutariya_official/?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+				// },
+				{
+					text: "Buy me a coffee",
+					link: "https://www.buymeacoffee.com/PragneshSutariya",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Pragnesh Sutariya.",
+				"&copy; No Copyrights. Feel free to use this template."
+			],
+		},
+	],
+};
